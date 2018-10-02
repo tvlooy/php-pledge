c294486007f0d2f2a71fe6c082cf2922f2c8c353
Tom Van Looy <tom@ctors.net>
2018-10-02 23:12:18 +0200

    Add support for unveil

--- php_pledge.h
+++ php_pledge.h
@@ -1,10 +1,11 @@
 /* php_pledge.h */
 
 #define PHP_PLEDGE_EXTNAME "pledge"
-#define PHP_PLEDGE_VERSION "1.0.0"
+#define PHP_PLEDGE_VERSION "2.0.0"
 
 extern zend_module_entry pledge_module_entry;
 #define phpext_pledge_ptr &check_pledge_entry
 
 PHP_FUNCTION(pledge);
+PHP_FUNCTION(unveil);
 
