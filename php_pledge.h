51f57059c307e23970852c30e2688be75411f792
Tom Van Looy <tom@ctors.net>
2018-10-12 23:42:37 +0200

    Bump

--- php_pledge.h
+++ php_pledge.h
@@ -1,7 +1,7 @@
 /* php_pledge.h */
 
 #define PHP_PLEDGE_EXTNAME "pledge"
-#define PHP_PLEDGE_VERSION "2.0.0"
+#define PHP_PLEDGE_VERSION "2.0.1"
 
 extern zend_module_entry pledge_module_entry;
 #define phpext_pledge_ptr &check_pledge_entry
